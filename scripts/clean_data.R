1a2fddd3639aee6968c8255eab79a1b809f5e641
#David Kulig# <#dkulig@bu.edu#>
2025-03-31 16:25:26 -0400

    Built site for gh-pages

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -38,3 +38,12 @@ county_grad_data_clean <- county_graduation_data |>
   filter(GraduationRate != "NULL")
 
 write_rds(county_grad_data_clean, file = here::here("dataset", "county_grad_clean.rds"))
+
+all_groups <- data.frame(StudentGroup = c("Non Section 504", "Section 504", "All Students", "English Language Learners", "Non-English Language Learners", "Foster Care", "Non-Foster Care", "Female", "Gender X", "Male", "Highly Capable", "Non-Highly Capable", "Homeless", "Non-Homeless", "Low-Income", "Non-Low Income", "Migrant", "Non Migrant", "Military Parent", "Non Military Parent", "American Indian/ Alaskan Native", "Asian", "Black/ African American", "Hispanic/ Latino of any race(s)", "Native Hawaiian/ Other Pacific Islander", "Two or More Races", "White", "Students with Disabilities", "Students without Disabilities"          
+))
+combined_grad_data <- read_rds(here::here("dataset","combined_grad_income.rds"))
+combined_grad_income_expanded <- combined_grad_data |>
+  mutate(Row = row_number()) |>
+  pivot_wider(names_from = StudentGroup, values_from = StudentGroup)
+
+write_rds(combined_grad_income_expanded, file = here::here("dataset", "combined_grad_income_expanded.rds"))
