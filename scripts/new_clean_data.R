30079449cd809e0953328d6a69a815b0d7ffefe6
#David Kulig# <#dkulig@bu.edu#>
2025-04-07 11:42:05 -0400

    changed variable names to be more accurate

---
+++ scripts/new_clean_data.R
@@ -0,0 +1,34 @@
+library(readr)
+library(dplyr)
+
+final_merged_no_charter <- read_csv(here::here("dataset", "final_merged_no_charter.csv"))
+
+massachusetts_district_data <- final_merged_no_charter |>
+  select(-`...1`, -`District Code_y`) |>
+  rename(
+    District_Code = `District Code_x`,
+    Cohort_Size = `# in Cohort`,
+    Percent_Graduated = `% Graduated`,
+    Percent_Still_in_School = `% Still in School`,
+    Percent_Non_Grad_Completers = `% Non-Grad Completers`,
+    Percent_HS_Equiv = `% H.S. Equiv.`,
+    Percent_Dropped_Out = `% Dropped Out`,
+    Percent_Excluded = `% Permanently Excluded`,
+    Percent_American_Indian = `American Indian or Alaska Native`,
+    Percent_Asian = Asian,
+    Percent_Black = `Black or African American`,
+    Percent_Hispanic = `Hispanic or Latino`,
+    Percent_Multi_Race = `Multi-Race, Not Hispanic or Latino`,
+    Percent_Pacific_Islander = `Native Hawaiian or Other Pacific Islander`,
+    Percent_White = White,
+    Percent_Female = Female,
+    Percent_Male = Male,
+    SAT_Tests_Taken = `Tests Taken`,
+    SAT_Reading_Writing_Mean_Score = `Reading / Writing`,
+    SAT_Math_Mean_Score = Math,
+    Student_Teacher_Ratio = `Student / Teacher Ratio`,
+    Low_Income_Percent = `Low Income %.y`
+  ) |>
+  mutate(across(where(is.character), trimws))
+
+write_rds(massachusetts_district_data, file = here::here("dataset", "massachusetts_district_data.rds"))
